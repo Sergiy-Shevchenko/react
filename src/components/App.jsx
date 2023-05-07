43310dceeb63bebe0698520bf04b0d61b0d3e976
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-05-08 00:32:14 +0300

    Update App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import {data} from '../Data/users.js'
+import { AddUserForm } from './AddUserForm/AddUserForm.js';
+import {Userlist} from './User/Users'
+import { nanoid } from 'nanoid';
+
+
+
+class App extends Component   {
+  state = {
+    users: data,
+  };
+
+
+  deleteUser = userId => {
+    this.setState(prevState => 
+      ({users: prevState.users.filter(({id}) => id !== userId),}))};
+
+  addUser = (userData) => {
+    const newUser = {
+      id: nanoid(),
+      ...userData, 
+    }
+    this.setState(prevState => ({users: [newUser, ...prevState.users]}))
+  }
+      
+  render () {
+    const {users} = this.state
+  return (
+   <div>
+    {/* < Userlist users={users} deleteUser={this.deleteUser}/> */}
+    < AddUserForm addUser={this.addUser}/>
+   </div>
+  );
+};
+}
+
+export default App;
